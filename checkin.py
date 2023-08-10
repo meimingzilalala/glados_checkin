b46c3bf2977c407721297c413488e6e86b21bf74
meimingzilalala <57852246+meimingzilalala@users.noreply.github.com>
2023-08-10 12:40:31 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -18,7 +18,7 @@ def start():
     referer = 'https://glados.rocks/console/checkin'
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-    print(res)
+    print(checkin)
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
