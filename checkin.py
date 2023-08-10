8db4d2ad93810a6d43ff604fb61e06b38404059c
meimingzilalala <57852246+meimingzilalala@users.noreply.github.com>
2023-08-10 12:34:57 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -24,7 +24,7 @@ def start():
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        #print(time)
+        print(time)
         if sever == 'on':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
     else:
