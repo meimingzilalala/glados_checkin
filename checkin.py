e92ad8022884b222dffd8d66f01d413cbcf4f43d
meimingzilalala <57852246+meimingzilalala@users.noreply.github.com>
2024-04-15 14:50:03 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -17,15 +17,16 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer },data={"token": "glados.one" })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer })
+    # checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer },data={"token": "glados.one" })
+    checkin = requests.post(url,headers={'cookie': cookie },data={"token": "glados.one" })
+    state =  requests.get(url2,headers={'cookie': cookie })
     print(checkin.json())
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
-        # time = state.json()['data']['leftDays']
-        # time = time.split('.')[0]
-        # print(time)
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        print(time)
         time = 100
         if sever == 'on':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
