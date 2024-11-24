b46a2b356c03146e0601e8c061a47a66203ab2ac
meimingzilalala <57852246+meimingzilalala@users.noreply.github.com>
2024-11-24 15:30:23 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -28,7 +28,7 @@ def start():
         time = time.split('.')[0]
         print(time)
         if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，' + time)
     else:
         requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=cookie过期')
 
